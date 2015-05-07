d2deb2eceebe3e921bc186a2cc3443f90a38336e
David Karam <david@posttool.com>
2015-05-07 11:10:15 -0700

    transcribe w/ dictionary

--- app/corpus/transcribe.js
+++ app/corpus/transcribe.js
@@ -1,9 +1,22 @@
+var fs = require('fs');
 var utils = require('../utils');
-var CmuDict = require('../phone/cmu/cmudict')
-var d = new CmuDict();
-d.on('ready', function () {
-  utils.readLines(__dirname + '/template-0.txt', function (data) {
-    console.log(d.getTranscription(data));
-  }, function () {
+var CmuDict = require('../phone/cmu/cmudict');
+function transcribe(infile, outfile, complete) {
+  var stream = fs.createWriteStream(outfile);
+  stream.once('open', function (fd) {
+    var d = new CmuDict();
+    d.on('ready', function () {
+      utils.readLines(infile, function (data) {
+        stream.write(d.getTranscription(data) + '\n');
+      }, function () {
+        stream.end();
+        complete();
+      });
+    });
   });
-});
+}
+
+
+transcribe(__dirname + '/template-0.txt', __dirname + '/template-0.ph', function () {
+  console.log("!");
+})
