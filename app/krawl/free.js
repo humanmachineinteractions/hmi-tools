7103a9472fb1364faecd01214409a48704253c74
David Karam <david@posttool.com>
2015-05-19 13:34:29 -0700

    use Stream ... add sports query

--- app/krawl/free.js
+++ app/krawl/free.js
@@ -1,13 +1,27 @@
 var freebase = require('freebase');
 var APIKEY = 'AIzaSyCoRa-naBDIOmwg71Q7WgqZQVv2XvvUmms';
 
-var outfile = 'data/artists.txt';
+var Stream = require('./filestream').Stream;
+
+var outfile = 'data/teams.txt';
 var query = [{
   "id": null,
   "name": null,
-  "type": "/music/artist",
-  "limit": 500
+  "type": "/sports/sports_team",
+  "location": [{
+    "id": null,
+    "name": null
+  }],
+  "limit": 100
 }];
+
+//var outfile = 'data/artists.txt';
+//var query = [{
+//  "id": null,
+//  "name": null,
+//  "type": "/music/artist",
+//  "limit": 500
+//}];
 
 //var outfile = 'data/albums.txt';
 //var query = [{
@@ -43,39 +57,33 @@ var query = [{
 //}]
 
 
-var fs = require('fs');
-var stream = fs.createWriteStream(outfile);
-stream.once('open', function (fd) {
-  console.log('opened', fd);
+new Stream(outfile, function (stream) {
+
+  var cursor = null;
+  var ids = {};
+  var c = 0;
+
+  function next() {
+    freebase.mqlread(query, {cursor: cursor, key: APIKEY}, function (r) {
+      r.result.forEach(function (b) {
+        if (!ids[b.id])
+          ids[b.id] = 1;
+        else {
+          ids[b.id]++;
+          console.log(b.id, ids[b.id])
+          return;
+        }
+        if (b.name != null) {
+          //console.log("* ",b);
+          stream.writeln(b.name);
+          c++;
+        }
+      })
+      cursor = r.cursor;
+      process.nextTick(next);
+    });
+  }
   next();
 });
 
-var cursor = null;
-var ids = {};
-var c = 0;
-function next() {
-  freebase.mqlread(query, {cursor: cursor, key: APIKEY}, function (r) {
-    r.result.forEach(function (b) {
-      if (!ids[b.id])
-        ids[b.id] = 1;
-      else {
-        ids[b.id]++;
-        console.log(b.id, ids[b.id])
-        return;
-      }
-      if (b.name != null)
-        stream.write(b.name + "\n");
-      //console.log(b.name);
-      c++;
-    })
-    cursor = r.cursor;
-    process.nextTick(next);
-  });
-}
-
-//freebase.related("gumby", {}, function (r) {
-//  console.log("!", r.map(function (v) {
-//    return v.name
-//  }))
-//});
 
