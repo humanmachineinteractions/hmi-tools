1fbde79202500c18cc9940189ebabb81162040be
David Karam <david@posttool.com>
2015-07-14 14:14:39 -0700

    more phone feats from fest

---
+++ app/phone/fest/wordtrans1.scm
@@ -0,0 +1,22 @@
+(load (path-append libdir "init.scm"))
+
+(define (pp fw fn)
+  (set! t (format nil "%s\n" fn)) ; scheme sux
+  (let ((u (utt.load nil fn)))
+    (mapcar (lambda (seg) (rsegs fw seg)) (utt.relation.items u 'SylStructure))
+    (format fw "\n")
+    t
+  )
+)
+
+(define (rsegs fw seg)
+  (set! n (item.feat seg 'name))
+  (set! b (item.feat seg 'segment_start))
+  (set! e (item.feat seg 'segment_end))
+  (set! t (item.feat seg 'R:SylStructure.tobi_endtone))
+  (set! d (item.feat seg 'R:SylStructure.daughtern.daughtern.end))
+  (format fw "%f %f %f %s \n" b e d n )
+)
+
+
+(pp (fopen "-" "w") (car argv))
